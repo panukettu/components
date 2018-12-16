ca5eabe299353288ed258af39e4e076b0a061745
panukettu <panu.t.kettunen@gmail.com>
2018-12-16 21:57:02 +0200

    created a tabs component

---
+++ src/Tabs.js
@@ -0,0 +1,71 @@
+import React from "react";
+const TabContext = React.createContext();
+
+function TabConsumer(props) {
+  return (
+    <TabContext.Consumer {...props}>
+      {context => props.children(context)}
+    </TabContext.Consumer>
+  );
+}
+
+export default class Tabs extends React.Component {
+  state = {
+    activeIndex: 0
+  };
+
+  static TabList = props => {
+    return (
+      <TabConsumer>
+        {context => {
+          const children = React.Children.map(
+            props.children,
+            (child, index) => {
+              return React.cloneElement(child, {
+                isActive: index === context.activeIndex,
+                setActive: context.setActive,
+                childIndex: index
+              });
+            }
+          );
+          return children;
+        }}
+      </TabConsumer>
+    );
+  };
+
+  static Tab = ({ isActive, childIndex, setActive, children }) => {
+    return (
+      <div
+        className={`tabs__heading${isActive ? "--active" : ""}`}
+        onClick={() => setActive(childIndex)}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  static Panels = ({ children }) => {
+    return (
+      <TabConsumer>{context => children[context.activeIndex]}</TabConsumer>
+    );
+  };
+
+  static Panel = ({ children }) => {
+    return <div className="tabs__content">{children}</div>;
+  };
+
+  setActive = index => {
+    this.setState({ activeIndex: index });
+  };
+
+  render() {
+    const { activeIndex } = this.state;
+    const contextValue = { activeIndex, setActive: this.setActive };
+    return (
+      <TabContext.Provider value={contextValue}>
+        {this.props.children}
+      </TabContext.Provider>
+    );
+  }
+}
